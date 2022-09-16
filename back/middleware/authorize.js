affedcd58f247393d95e38570a974734310cdf59
MDI <amaloumehdi6@gmail.com>
2022-09-16 18:08:06 +0200

    add authorize(verify token)

---
+++ back/middleware/authorize.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+// Variable d'environnement
+require('dotenv').config();
+
+module.exports = (req, res, next) => {
+    try{
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, `${process.env.token_key}`);
+        const userId = decodedToken.userId;
+            req.authorise = {
+                userId: userId
+            };
+        next();
+    } catch(error) {
+        res.status(401).json({ error });
+    }
+};
