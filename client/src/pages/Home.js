e7a4f2014940e2e3f40ef8542139b8fd7774cd7a
MDI <amaloumehdi6@gmail.com>
2022-10-05 12:33:43 +0200

    Add new element 'home page' & 'posts page'

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,26 @@
 import React from "react";
+import Logo from '../assets/Logos/icon-left-font-monochrome-black.svg'
+import styled from "styled-components";
 
 function Home() {
     return( 
         <div>
-            <h1>Bienvenue sur la page d'accueil</h1>
+            <StyledHeader>
+                    <StyledLogo src={Logo} alt="Logo de l'entreprise Groupomania" />
+            </StyledHeader> 
         </div>
     )
 }
+
+const StyledHeader = styled.span`
+    display: flex;
+    justify-content: center;
+    margin-bottom: 100px;
+`
+const StyledLogo = styled.img`
+    height: 30px;
+    padding: 15px;
+    display: flex;
+`
 
 export default Home;
--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -1,9 +1,25 @@
 import React from "react";
+import Logo from '../assets/Logos/icon-left-font-monochrome-black.svg'
+import styled from "styled-components";
+
+const StyledHeader = styled.span`
+    display: flex;
+    justify-content: center;
+    margin-bottom: 100px;
+`
+
+const StyledLogo = styled.img`
+    height: 30px;
+    padding: 15px;
+    display: flex;
+`
 
 function NewPost() {
     return(
         <div>
-            <h1>Bienvenue sur créer un post</h1>
+            <StyledHeader>
+                <StyledLogo src={Logo} alt="Logo de l'entreprise Groupomania" />
+            </StyledHeader> 
         </div>
     )
 }
