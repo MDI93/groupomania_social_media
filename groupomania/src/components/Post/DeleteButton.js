f300e0fa40444a0df66ad5248fea68f4e9c0a4f7
MDI <amaloumehdi6@gmail.com>
2022-10-24 08:41:57 +0200

    add delete button

--- groupomania/src/components/Post/DeleteButton.js
+++ groupomania/src/components/Post/DeleteButton.js
@@ -1,28 +1,54 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useContext, useState } from 'react'
 import styled from 'styled-components';
+import { AuthUserContext } from '../../context/UserContext';
 
-const DeleteButton = () => {
-  const deleteQuote = () => {
-    console.log("test supprime")
-  }
+const DeleteButton = ({ post }) => {
+  const authId = useContext(AuthUserContext);
+  const isLoggedIn = authId.isLoggedIn;
+  const [deletePost, setDeletePost] = useState(false);
+  const [errorMessage, setErrorMessage] = useState();
 
-  const deleteHandler = () => {
-
-  }
+  const deleteHandler = (e) => {
+    console.log("------delete Post", deleteHandler )
+    axios.delete(`${process.env.REACT_APP_URL_API}/posts/${post._id}`, {
+      headers:{
+        Authorization: `Bearer ${localStorage.getItem("token")}`
+      },
+      data: { post }
+    })
+    .then((res) => {
+      console.log(res)
+      if(res.error) {
+        setErrorMessage(res.error);
+      } else {
+          setDeletePost(false);
+      }
+    })
+    .catch((error) => console.log(error))
+  };
+  console.log("delete post", deleteHandler)
 
   return (
-    <StyledBtnDiv onClick={() => {
-      if(window.confirm('Voulez-vous vraiment supprimer cet article ?')){
-        deleteQuote();
-      }
-    }}>
-        <StyledBtnCard className="card-btn-delete" alt="Bouton pour supprimer l'article">
-                <i class="fa-solid fa-trash"></i>
+    <>
+    {isLoggedIn && (
+      <StyledBtnDiv>
+        <StyledBtnCard 
+          onClick={() => {if(window.confirm('Voulez-vous vraiment supprimer cet article ?')){
+            deleteHandler();}
+          }}
+          onChange={(e) => setDeletePost(e.target.value)}
+          className="card-btn-delete" 
+          alt="Bouton pour supprimer l'article"
+          value={deletePost}
+          >
+          <i class="fa-solid fa-trash"></i>
         </StyledBtnCard>
-    </StyledBtnDiv>
+      </StyledBtnDiv>
+      )}
+    </>
     
-  )
-}
+  )};
 
 export default DeleteButton;
 
